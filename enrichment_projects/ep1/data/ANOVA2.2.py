8a785cf361083214d0aeb53ebc1280900d2de06b
Akshay Govindarj <akshaygvdrj@gmail.com>
2018-09-22 17:17:12 -0400

    F- Test
    Both tests reject H0

--- enrichment_projects/ep1/data/ANOVA2.2.py
+++ enrichment_projects/ep1/data/ANOVA2.2.py
@@ -8,6 +8,7 @@ Created on Sat Sep 22 12:26:44 2018
 
 import pandas as pd
 
+from scipy.stats import f_oneway
 from scipy.stats import friedmanchisquare
 
 stock_df = pd.read_csv('/Users/akshaygovindaraj/Documents/GitHub/isye-6404-fall18-proj/enrichment_projects/ep1/data/Friedman.csv')
@@ -17,6 +18,20 @@ treatment2 = stock_df.values[:,2]
 treatment3 = stock_df.values[:,3]
 
 stat, p = friedmanchisquare(treatment1, treatment2, treatment3)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Same distributions (fail to reject H0)')
+else:
+	print('Different distributions (reject H0)')
+    
+    
+#Performing the f test
+
+stat, p = f_oneway(treatment1, treatment2, treatment3)
+
 print('Statistics=%.3f, p=%.3f' % (stat, p))
 
 # interpret
