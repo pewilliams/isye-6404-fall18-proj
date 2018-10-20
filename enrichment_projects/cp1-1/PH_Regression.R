ad0ed52b43f4aa4042a02b7114ec7e63c40ffafd
Akshay Govindarj <akshaygvdrj@gmail.com>
2018-10-20 11:05:35 -0400

    summary of ph regression
    Please install survival package to run script directly.NO need to download dataset.

--- enrichment_projects/cp1-1/PH_Regression.R
+++ enrichment_projects/cp1-1/PH_Regression.R
@@ -6,3 +6,4 @@ disease = cbind(with(kidney, model.matrix(~ disease + 0)))
 df = as.data.frame(disease)
 
 res.cox <- coxph(Surv(kidney$time, kidney$status) ~ kidney$age + kidney$sex + kidney$frail + df$diseaseOther + df$diseaseGN + df$diseaseAN )
+summary(res.cox)
