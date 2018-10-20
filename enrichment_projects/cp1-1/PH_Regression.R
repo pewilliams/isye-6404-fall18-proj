52494d45abfa13e27c4ac16191a6ee84918e0739
Akshay Govindarj <akshaygvdrj@gmail.com>
2018-10-20 13:09:33 -0400

    Unifying dataframe
    Please install survival and survminer before running script

--- enrichment_projects/cp1-1/PH_Regression.R
+++ enrichment_projects/cp1-1/PH_Regression.R
@@ -1,9 +1,13 @@
 
 library("survival")
+library("survminer")
 data("kidney")
 
 disease = cbind(with(kidney, model.matrix(~ disease + 0)))
 df = as.data.frame(disease)
+df_kidney = merge(kidney, df)
 
-res.cox <- coxph(Surv(kidney$time, kidney$status) ~ kidney$age + kidney$sex + kidney$frail + df$diseaseOther + df$diseaseGN + df$diseaseAN )
+res.cox <- coxph(Surv(time, status) ~ age + sex + frail + diseasePKD + diseaseGN + diseaseAN , data = df_kidney)
 summary(res.cox)
+
+ggsurvplot(survfit(res.cox), palette = "#2E9FDF",ggtheme = theme_minimal(), data = df_kidney)
