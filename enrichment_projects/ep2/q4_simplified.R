6838f3a7adddd60837d39479d649044e7ac577e2
AkshayGovindaraj <31434548+AkshayGovindaraj@users.noreply.github.com>
2018-11-17 14:20:31 -0500

    Stand error and Bias

--- enrichment_projects/ep2/q4_simplified.R
+++ enrichment_projects/ep2/q4_simplified.R
@@ -9,11 +9,6 @@ km_dat <- data.frame(time=km_fit$time,
                      upper_ci = km_fit$upper, 
                      lower_ci = km_fit$lower) #model results
 melted_km_dat <- melt(km_dat, id.vars = c('time')) #transform for viz
-# ggplot(aes(x=time,y=value, color = variable ,group = variable), data=melted_km_dat) +
-#   geom_step(size = 1.25) + theme_bw() + xlab('Time (days)') + 
-#   ylab('Survival Function') + 
-#   ggtitle('K-M Estimates with 95% Confidence Bounds: Heart Transplant Patients') + 
-#   labs(color='Curve')
 
 #Finding the Standard Error and survival
 s_error = summary(km_fit)$std.err
@@ -29,6 +24,9 @@ km_dat1 <- data.frame(time=km_fit$time,
 
 melted_km_dat1 <- melt(km_dat1, id.vars = c('time'))
 
+# Table for 1st version of CI
+
+
 #ggplot(aes(x=time,y=value, color = variable ,group = variable), data=melted_km_dat1) +
 #  geom_step(size = 1.25) + theme_bw() + xlab('Time (days)') + 
 #  ylab('Survival Function') + 
@@ -38,19 +36,23 @@ melted_km_dat1 <- melt(km_dat1, id.vars = c('time'))
 ############Function for bootstrapping Estimates for the Median 
 km_est_boot <- function(data, indices){
   bs_dat <- data[indices,]
-#  res.cox <- coxph(Surv(time, status) ~ age + sex + frail , data = bs_dat)
-#  return(c(as.numeric(res.cox$coefficients["age"])))
   km_fitbs <- survfit(Surv(time = Days, event = Status) ~ 1, data = bs_dat, 
-                    type = 'kaplan-meier') #only one group of patients
+                      type = 'kaplan-meier') #only one group of patients
   km_datbs <- data.frame(time=km_fitbs$time,
                          survival=km_fitbs$surv) #model results
-  return(median(km_datbs$survival))
-### INCOMPLETE
+  return(median(km_datbs$time))
+  ### INCOMPLETE
 }
 library(boot)
 B <- 1e04
 bs_CI <- boot(hdat, km_est_boot, R=B,parallel = 'multicore',
-                                    ncpus = 10)
+              ncpus = 10)
 #plot(bs_CI)
 
 boot.ci(boot.out = bs_CI,type = 'bca')
+
+# Standard error of Bootstrap samples of Median
+se <- sd(bs_CI$t)
+
+#Bias calculation (Mean of bootstrap estimates - Median of KM estimator)
+bias <- mean(bs_CI$t) - median(km_dat$time)
